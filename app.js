7c541432e2911a43c2829f1b18c02d7b457e7eb7
Buck Doyle <b@chromatin.ca>
2019-03-10 22:15:05 -0500

    Add CORS support

--- app.js
+++ app.js
@@ -109,6 +109,12 @@ function wordFinder(boardString, words) {
   
 var words = ['ab', 'ad', 'bad'];
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', function (req, res) {
     res.json(wordFinder('abcd', words));
 });
